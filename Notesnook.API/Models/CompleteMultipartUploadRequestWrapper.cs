30d53944255c0d4c94dff47955671f63232653c8
dyw770 <dyw770@qq.com>
2025-08-18 16:33:49 +0800

    api: fix s3 multipart upload (#22)
    * api: fix s3 multipart upload use external s3 url
    
    * api: fix s3 multipart upload use external s3 url
    
    * api: fix CompleteMultipartUploadRequest can not deserialize
    
    * api: start multipart upload use s3 internal url
    
    * Update Notesnook.API/Models/PartETagWrapper.cs
    
    remove default constructor
    
    Co-authored-by: Abdullah Atta <thecodrr@protonmail.com>
    
    * api: remove default constructor
    
    Co-authored-by: Abdullah Atta <thecodrr@protonmail.com>
    
    * api: merge method call
    
    Co-authored-by: Abdullah Atta <thecodrr@protonmail.com>
    
    * api: revocation due to conflict
    
    * api: revocation due to conflict
    
    ---------
    
    Co-authored-by: Abdullah Atta <thecodrr@protonmail.com>

---
+++ Notesnook.API/Models/CompleteMultipartUploadRequestWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Amazon.S3.Model;
+
+namespace Notesnook.API.Models;
+
+public class CompleteMultipartUploadRequestWrapper
+{
+    public string Key { get; set; }
+    public List<PartETagWrapper> PartETags { get; set; }
+    public string UploadId { get; set; }
+
+    public CompleteMultipartUploadRequest ToRequest()
+    {
+        CompleteMultipartUploadRequest completeMultipartUploadRequest = new CompleteMultipartUploadRequest();
+        completeMultipartUploadRequest.Key = Key;
+        completeMultipartUploadRequest.UploadId = UploadId;
+        completeMultipartUploadRequest.PartETags = [];
+        foreach (var partETagWrapper in PartETags)
+        {
+            var partETag = new PartETag
+            {
+                PartNumber = partETagWrapper.PartNumber,
+                ETag = partETagWrapper.ETag
+            };
+            completeMultipartUploadRequest.PartETags.Add(partETag);
+        }
+
+        return completeMultipartUploadRequest;
+    }
+}
---
+++ Notesnook.API/Models/PartETagWrapper.cs
@@ -0,0 +1,7 @@
+﻿namespace Notesnook.API.Models;
+
+public class PartETagWrapper
+{
+    public int PartNumber { get; set; }
+    public string ETag { get; set; }
+}
