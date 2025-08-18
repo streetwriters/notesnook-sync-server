0bc8a6683da1b00e94980ca34d3bffaefbc7b913
dyw770 <dyw770@qq.com>
2025-08-18 14:58:11 +0800

    Update Notesnook.API/Models/PartETagWrapper.cs
    remove default constructor
    
    Co-authored-by: Abdullah Atta <thecodrr@protonmail.com>

--- Notesnook.API/Models/PartETagWrapper.cs
+++ Notesnook.API/Models/PartETagWrapper.cs
@@ -4,11 +4,4 @@ public class PartETagWrapper
 {
     public int PartNumber { get; set; }
     public string ETag { get; set; }
-
-    /// <summary>
-    /// Default constructor.
-    /// </summary>
-    public PartETagWrapper()
-    {
-    }
 }
