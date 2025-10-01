d72ceabdcbf6e869c2dc3c4d15a5afac73d4b322
Abdullah Atta <abdullahatta@streetwriters.co>
2025-10-01 12:00:01 +0500

    common: handle paddle billing errors

--- Streetwriters.Common/Interfaces/IUserAccountService.cs
+++ Streetwriters.Common/Interfaces/IUserAccountService.cs
@@ -7,7 +7,7 @@ namespace Streetwriters.Common.Interfaces
     public interface IUserAccountService
     {
         [WampProcedure("co.streetwriters.identity.users.get_user")]
-        Task<UserModel> GetUserAsync(string clientId, string userId);
+        Task<UserModel?> GetUserAsync(string clientId, string userId);
         [WampProcedure("co.streetwriters.identity.users.delete_user")]
         Task DeleteUserAsync(string clientId, string userId, string password);
         // [WampProcedure("co.streetwriters.identity.users.create_user")]
